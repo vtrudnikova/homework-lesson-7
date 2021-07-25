639806c52113fa77a692b87d71c852d6f88f403e
vtrudnikova <violetta8trudnikova@gmail.com>
2021-07-25 17:56:42 +0300

    commit new

---
+++ src/test/java/guru/qa/TestGit.java
@@ -0,0 +1,14 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Test;
+
+public class TestGit {
+    @Test
+    public void firstTest(){
+        System.err.println("test");
+    }
+    @Test
+    public void secondTest(){
+        System.err.println("test");
+    }
+}
